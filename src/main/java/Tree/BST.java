88dc108141d2acfdcac5accfb46e9a67946c5d7e
whl6785968 <806403789@qq.com>
2019-08-16 21:05:43 +0800

    currently learning page 282

--- src/main/java/Tree/BST.java
+++ src/main/java/Tree/BST.java
@@ -94,6 +94,84 @@ public class BST<Key extends Comparable<Key>,Value> {
         else return x;
     }
 
+    public Key select(int k){
+        return select(root,k).key;
+    }
+
+    public Node select(Node x,int k){
+        if(x == null) return null;
+
+        int t = size(x.left);
+        if(t>k){
+            return select(x.left,k);
+        }
+        else if(t<k){
+            return select(x.right,k-t-1);
+        }
+        else return x;
+    }
+
+    public int rank(Key key){
+        return rank(root,key);
+    }
+
+    public int rank(Node x,Key key){
+        if(x == null) return 0;
+
+        int cmp = key.compareTo(x.key);
+        if(cmp<0){
+            return rank(x.left,key);
+        }
+        else if(cmp>0){
+            return 1 + size(x.left) + rank(x.right,key);
+        }
+        else {
+            return size(x.left);
+        }
+    }
+
+    public Key deleteMin(){
+        return deleteMin(root).key;
+    }
+
+    public Node deleteMin(Node x){
+        if(x.left == null){
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Key delete(Key key){
+        return delete(root,key).key;
+    }
+
+    public Node delete(Node x,Key key){
+        int cmp = key.compareTo(x.key);
+
+        if(cmp<0){
+            x.left = delete(x.left,key);
+            return delete(x.left,key);
+        }
+        else if(cmp>0){
+            x.right = delete(x.right,key);
+            return delete(x.right,key);
+        }
+        else {
+            if(x.right == null) return x.left;
+            if(x.left == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+
+    }
+
 
 
 
